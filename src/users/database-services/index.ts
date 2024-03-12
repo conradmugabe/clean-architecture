7e941039a23a1fb6c99b3da3719374928fa9e0e3
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 20:18:13 +0300

    feat: added docker

--- src/users/database-services/index.ts
+++ src/users/database-services/index.ts
@@ -1,6 +1,8 @@
 import { User } from '@/users/core';
 
 export abstract class UserDatabase {
+  abstract createUser(userId: string): Promise<User>;
+
   abstract findUsers(): Promise<User[]>;
 
   abstract findUserById(userId: string): Promise<User | null>;
--- src/users/user-use-cases/index.ts
+++ src/users/user-use-cases/index.ts
@@ -1,9 +1,21 @@
 import { User } from '@/users/core';
 import { UserDatabase } from '@/users/database-services';
 import { NotFoundError } from '@/utils/errors/not-found-error';
+import { BadRequestError } from '@/utils/errors/bad-request-error';
 
 export class UserUseCases {
   constructor(private database: UserDatabase) {}
+
+  async createUser(userId: string): Promise<User> {
+    const existingUser = await this.database.findUserById(userId);
+    if (existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const user = this.database.createUser(userId);
+
+    return user;
+  }
 
   async getUsers(): Promise<User[]> {
     const users = await this.database.findUsers();
