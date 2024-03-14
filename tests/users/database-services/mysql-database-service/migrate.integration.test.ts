f897fffdcf82bf77eadc18c960400361e33d2e91
conrad mugabe <conradmugabe@gmail.com>
2024-03-14 22:13:06 +0300

    tests: added tests for mysql database connector migrate

---
+++ tests/users/database-services/mysql-database-service/migrate.integration.test.ts
@@ -0,0 +1,64 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest';
+import { MySqlContainer, StartedMySqlContainer } from '@testcontainers/mysql';
+
+import { migrate } from '@/users/database-services/mysql-database-service/migrate';
+
+let container: MySqlContainer = new MySqlContainer();
+let startedContainer: StartedMySqlContainer;
+
+const SIXTY_SECONDS = 60 * 1000;
+
+beforeAll(async () => {
+  startedContainer = await container.start();
+}, SIXTY_SECONDS);
+
+afterAll(async () => {
+  await startedContainer.stop();
+}, SIXTY_SECONDS);
+
+describe('Test Mysql Database Connector Migrate', () => {
+  const errorPort = 1000;
+
+  interface TestCases {
+    name: string;
+    connectionUri?: string;
+    error?: Error;
+  }
+
+  const testCases: TestCases[] = [
+    {
+      name: 'errors when an invalid connection string is provided',
+      connectionUri: 'random string',
+      error: new Error('Invalid URL'),
+    },
+    {
+      name: 'errors when failed to connect to database',
+      connectionUri: `mysql://localhost:${errorPort}`,
+      error: new Error(`connect ECONNREFUSED ::1:${errorPort}`),
+    },
+    {
+      name: 'migrates successfully',
+    },
+  ];
+
+  test.each(testCases)('$name', async (testCase) => {
+    const connectionUri =
+      testCase.connectionUri || startedContainer.getConnectionUri();
+
+    try {
+      const db = await migrate(connectionUri);
+
+      if (testCase.error) {
+        expect.fail('test case should throw an error');
+      }
+
+      expect(db).toBeDefined();
+    } catch (error) {
+      if (!testCase.error) {
+        expect.fail('test case should not throw an error');
+      }
+
+      expect(error).toEqual(testCase.error);
+    }
+  });
+});
